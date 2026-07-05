cae29316c77e4e07e7f00ba620414313bb4a6be6
5VN5H <shivansh.1111111@gmail.com>
2026-07-05 15:40:27 +0530

    Time: 2 ms (46.95%), Space: 12.9 MB (39.5%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        int n = nums2.size();
+
+        stack<int> st;
+        vector<int> nge(n);
+
+        // Find Next Greater for nums2
+        for (int i = n - 1; i >= 0; i--) {
+
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
+
+            if (st.empty())
+                nge[i] = -1;
+            else
+                nge[i] = st.top();
+
+            st.push(nums2[i]);
+        }
+
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[nums2[i]] = nge[i];
+        }
+
+        vector<int> ans;
+
+        for (int x : nums1) {
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
+    }
+};
