036076e6262152555699fe3c13ed4809200f3042
5VN5H <shivansh.1111111@gmail.com>
2026-08-05 14:25:02 +0530

    Time: 0 ms (100%), Space: 7.9 MB (49.47%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int n,int k){
+
+        int mid = (pow(2,n-1))/2;
+
+        if(n==1){
+            return 0;
+        }
+
+        if(k<=mid){
+            return solve(n-1,k);
+        }
+        return 1-solve(n-1,k-mid);
+    }
+    int kthGrammar(int n, int k) {
+        return solve(n,k);
+    }
+};
